1ea3f88c70db7643ce490da5e952de8bc35856d9
Hermitcrabb <prathambhandari2000@gmail.com>
2025-05-11 16:08:59 +0545

    Start of our project

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(mero_swastha());
+
+class mero_swastha extends StatefulWidget{
+  const mero_swastha({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
+  @override
+
+
+}
